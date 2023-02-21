255ff855dad77cbcbf43d6e6bfc9b086b14f7fea
ejmaza88 <esvin@fimed.com>
2023-02-21 13:28:23 -0600

    Auth app

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
 
     # Local
+    'appauth.apps.AppauthConfig',
     'quickstart.apps.QuickstartConfig'
 ]
 
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-
 urlpatterns = [
+    # api
     path('', include('quickstart.urls')),
 
     # django admin
     path('admin/', admin.site.urls),
 
-
-    # sjwt
-    path('token/', TokenObtainPairView.as_view(), name='token-pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    # auth
+    path('auth/', include('appauth.urls'))
 ]
