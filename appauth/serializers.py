7595070f5ab7cacf073eb8bbc1a1704ad8bc458a
ejmaza88 <esvin@fimed.com>
2023-03-03 15:15:10 -0600

    Overriding validate on serializer

--- appauth/serializers.py
+++ appauth/serializers.py
@@ -3,6 +3,13 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class TokenObtainPairCustomSerializer(TokenObtainPairSerializer):
 
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # ... Add initial state here maybe??
+
+        return data
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
